03936192f6afa47635e086e56787394fd8c0981c
Artem Dymov <dymov.artem@outlook.com>
2023-01-23 18:10:40 +0200

    updated token

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = '5405581758:AAFTEo3yo-BwD2uMKpkEeHjgnqmrE0GG1pc'
+BOT_TOKEN = '5129250566:AAHMo-esT4W7SZQ_Cd-jnuBuTEEidlLC9zI'
 
 skip_message = 'Щоб пропустити це питання, натисніть /skip'
 
